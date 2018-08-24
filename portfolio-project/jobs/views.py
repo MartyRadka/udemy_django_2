b7b0b8740a1e65b8a285bfced8b7badc2dcfb124
Radka Marty <martyradka@gmail.com>
2018-08-24 15:36:26 +0200

    Added bootstrap and jobs are showing up

--- portfolio-project/jobs/views.py
+++ portfolio-project/jobs/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from .models import Job
 
 def home(request):
-    return render(request, 'home.html')
+    # jobs/home it's because we can have 2 home, it prevents the collision
+    # we want to present our instances of Job objects so we must only get this
+    # instances from our models and put them into the dict which we return in render
+
+    jobs = Job.objects
+
+    return render(request, 'jobs/home.html', {'jobs': jobs})
