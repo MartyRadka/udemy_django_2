14595bdaa7fb099fccbfd7fec386dba62f95b1e4
Radka Marty <martyradka@gmail.com>
2018-08-30 11:12:36 +0200

    Create new project Product hunt, set the database postgreSQL

--- producthunt/producthunt/urls.py
+++ producthunt/producthunt/urls.py
@@ -1,4 +1,4 @@
-"""product_hunt URL Configuration
+"""producthunt URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.1/topics/http/urls/
