942220446d0a3dd40512e15d2778eb8abb101af8
Radka Marty <martyradka@gmail.com>
2018-08-30 11:26:02 +0200

    add two new apps into project

--- producthunt/producthunt/settings.py
+++ producthunt/producthunt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from .private_config import secret_key, user_postgresql, password_postgresql
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
